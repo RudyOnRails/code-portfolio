bb6f33450b027bf1f09223e7d6c4f40583d65a09
Kevin Musiorski <rudyonrails@gmail.com>
2017-08-10 07:55:16 -0500

    reset

--- chitech/application_helper.rb
+++
@@ -1,7 +0,0 @@
-module ApplicationHelper
-  def title
-    prepend = "DEV: " if Rails.env == "development"
-    chitech = "Chicago Tech Academy"
-    prepend ? prepend + chitech : chitech
-  end
-end
